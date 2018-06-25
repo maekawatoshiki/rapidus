10f1762357ce9b9dab5e47cd1f240816a8146355
maekawatoshiki <konndennsa@gmail.com>
2018-06-25 20:27:12 +0900

    use clap for command line parser

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,19 @@
+extern crate rapidus;
+
+extern crate clap;
+use clap::{App, Arg};
+
+const VERSION_STR: &'static str = env!("CARGO_PKG_VERSION");
+
 fn main() {
-    println!("Hello, world!");
+    let app = App::new("Rapidus")
+        .version(VERSION_STR)
+        .author("uint256_t")
+        .about("A toy JavaScript engine")
+        .arg(Arg::with_name("file").help("Input file name").index(1));
+    let app_matches = app.clone().get_matches();
+
+    if let Some(filename) = app_matches.value_of("file") {
+        println!("filename: {}", filename);
+    }
 }
