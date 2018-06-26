425639439d9fa00db8c417d86a14b23d5efe7781
maekawatoshiki <konndennsa@gmail.com>
2018-06-26 19:10:53 +0900

    update parser

--- src/parser.rs
+++ src/parser.rs
@@ -4,13 +4,14 @@ pub struct Parser {
     pub lexer: lexer::Lexer,
 }
 
-pub struct Node {
+pub enum Node {
+    StatementList(Vec<Node>),
 }
 
 impl Parser {
-    pub fn new(code:String) -> Parser {
+    pub fn new(code: String) -> Parser {
         Parser {
-            lexer: lexer::Lexer::new(code)
+            lexer: lexer::Lexer::new(code),
         }
     }
 }
@@ -22,13 +23,41 @@ impl Parser {
 }
 
 impl Parser {
-    fn read_script(&mut self) -> Result<Node, ()> { 
+    fn read_script(&mut self) -> Result<(), ()> {
         Err(())
     }
 }
 
 impl Parser {
     fn read_statement_list(&mut self) -> Result<Node, ()> {
+        let mut items = vec![];
+
+        while let Ok(item) = self.read_statement_list_item() {
+            items.push(item)
+        }
+
+        Ok(Node::StatementList(items))
+    }
+
+    fn read_statement_list_item(&mut self) -> Result<Node, ()> {
+        if let Ok(true) = self.is_declaration() {
+            self.read_declaration()
+        } else {
+            self.read_statement()
+        }
+    }
+
+    fn read_statement(&mut self) -> Result<Node, ()> {
+        Err(())
+    }
+}
+
+impl Parser {
+    fn is_declaration(&self) -> Result<bool, ()> {
+        Ok(false)
+    }
+
+    fn read_declaration(&mut self) -> Result<Node, ()> {
         Err(())
     }
 }
