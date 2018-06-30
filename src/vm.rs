ccb49a2b8f79eb0aea980cd2acf8fcca35f48814
maekawatoshiki <konndennsa@gmail.com>
2018-07-01 00:16:46 +0900

    update vm

--- src/vm.rs
+++ src/vm.rs
@@ -1,1 +1,17 @@
+use std::collections::HashMap;
 
+pub enum Value {
+    Number(f64),
+    String(String),
+    Function(Vec<i32>), // i32 will replaced with appropriate type.
+    Object(HashMap<String, Object>),
+}
+
+pub struct Object {
+    pub val: Value,
+    pub name: Option<String>,
+}
+
+pub struct VM {
+    pub global_objects: HashMap<String, Object>,
+}
