b57083f6c5eb23539247bcff06e93eab665be496
maekawatoshiki <konndennsa@gmail.com>
2018-07-03 23:36:56 +0900

    struct Object is deleted

--- src/vm.rs
+++ src/vm.rs
@@ -14,61 +14,33 @@ pub enum Value {
 }
 
 #[derive(Clone, Debug, PartialEq)]
-pub struct Object {
-    pub name: Option<String>,
-    pub val: Value,
-}
-
-#[derive(Clone, Debug, PartialEq)]
 pub enum Inst {
-    Push(Object),
+    Push(Value),
     Member,
     GetGlobal(String),
     GetLocal(String),
-    SetGlobal(String, Object),
+    SetGlobal(String, Value),
     Call(usize),
 }
 
 pub struct VM {
-    pub global_objects: HashMap<String, Object>,
-    pub heap: HashMap<HeapAddr, Object>,
-    pub stack: VecDeque<Object>,
-}
-
-impl Object {
-    pub fn new(name: Option<String>, val: Value) -> Object {
-        Object {
-            name: name,
-            val: val,
-        }
-    }
-
-    pub fn new_no_name(val: Value) -> Object {
-        Object {
-            name: None,
-            val: val,
-        }
-    }
+    pub global_objects: HashMap<String, Value>,
+    pub heap: HashMap<HeapAddr, Value>,
+    pub stack: VecDeque<Value>,
 }
 
 impl VM {
     pub fn new() -> VM {
         let mut obj = HashMap::new();
         let console_log_addr = 0;
-        obj.insert(
-            "console".to_string(),
-            Object::new(Some("console".to_string()), {
-                let mut map = HashMap::new();
-                map.insert("log".to_string(), console_log_addr);
-                Value::Object(map)
-            }),
-        );
+        obj.insert("console".to_string(), {
+            let mut map = HashMap::new();
+            map.insert("log".to_string(), console_log_addr);
+            Value::Object(map)
+        });
 
         let mut heap = HashMap::new();
-        heap.insert(
-            console_log_addr,
-            Object::new(Some("log".to_string()), Value::EmbeddedFunction(1)),
-        );
+        heap.insert(console_log_addr, Value::EmbeddedFunction(1));
 
         VM {
             global_objects: obj,
@@ -83,8 +55,7 @@ impl VM {
         for inst in insts {
             match inst {
                 Inst::Push(val) => self.stack.push_back(val),
-                Inst::GetGlobal(name) => self
-                    .stack
+                Inst::GetGlobal(name) => self.stack
                     .push_back(self.global_objects.get(name.as_str()).unwrap().clone()),
                 Inst::SetGlobal(name, obj) => {
                     self.global_objects.insert(name, obj);
@@ -92,14 +63,14 @@ impl VM {
                 Inst::Member => {
                     let member_name = {
                         let member = self.stack.pop_back().unwrap();
-                        if let Value::Data(name) = member.val {
+                        if let Value::Data(name) = member {
                             name
                         } else {
                             panic!()
                         }
                     };
                     let parent = self.stack.pop_back().unwrap();
-                    if let Value::Object(map) = parent.val {
+                    if let Value::Object(map) = parent {
                         match map.get(member_name.as_str()) {
                             Some(v) => self.stack.push_back(self.heap.get(v).unwrap().clone()),
                             None => {}
@@ -119,8 +90,8 @@ impl VM {
         }
 
         let callee = self.stack.pop_back().unwrap();
-        match callee.val {
-            Value::EmbeddedFunction(1) => console_log(&args[0].val),
+        match callee {
+            Value::EmbeddedFunction(1) => console_log(&args[0]),
             _ => {}
         }
 
@@ -138,16 +109,13 @@ impl VM {
 pub fn test() {
     let insts = vec![
         Inst::GetGlobal("console".to_string()),
-        Inst::Push(Object::new_no_name(Value::Data("log".to_string()))),
+        Inst::Push(Value::Data("log".to_string())),
         Inst::Member,
-        Inst::Push(Object::new_no_name(Value::String("hello".to_string()))),
+        Inst::Push(Value::String("hello".to_string())),
         Inst::Call(1),
-        Inst::SetGlobal(
-            "n".to_string(),
-            Object::new(Some("n".to_string()), Value::Number(123.0)),
-        ),
+        Inst::SetGlobal("n".to_string(), Value::Number(123.0)),
         Inst::GetGlobal("console".to_string()),
-        Inst::Push(Object::new_no_name(Value::Data("log".to_string()))),
+        Inst::Push(Value::Data("log".to_string())),
         Inst::Member,
         Inst::GetGlobal("n".to_string()),
         Inst::Call(1),
