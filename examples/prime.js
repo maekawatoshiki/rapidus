f49c7e8128be4e02b92fd766f915e86eb4c7725f
maekawatoshiki <konndennsa@gmail.com>
2018-08-11 20:44:17 +0900

    edit an example

--- examples/prime.js
+++ examples/prime.js
@@ -1,18 +1,20 @@
 function prime(n) {
-  var k = 2
+  if (n % 2 == 0) return false;
+
+  var k = 3;
   while (k * k <= n) {
     if (n % k == 0) {
-      return false
+      return false;
     }
-    k += 1
+    k += 2
   }
-  return true
+  return true;
 }
 
-var i = 2
+var i = 2;
 while (i < 100000) {
   if (prime(i)) {
-    console.log(i, "is prime")
+    console.log(i, "is prime");
   }
-  i += 1
+  i += 1;
 }
