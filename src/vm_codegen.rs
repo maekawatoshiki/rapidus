f1ae98560015ed79c495047cf1a7dae14cd7e0f9
maekawatoshiki <konndennsa@gmail.com>
2018-09-18 23:19:53 +0900

    refine code

--- src/vm_codegen.rs
+++ src/vm_codegen.rs
@@ -583,6 +583,8 @@ impl VMCodeGen {
     }
 
     pub fn run_binary_op(&mut self, lhs: &Node, rhs: &Node, op: &BinOp, insts: &mut ByteCode) {
+        // Editing this (LAnd and LOr) source code has influence on the source code of JIT
+        // compiler(src/jit.rs).
         match op {
             &BinOp::LAnd => {
                 self.run(lhs, insts);
