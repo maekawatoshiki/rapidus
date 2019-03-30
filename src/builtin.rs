68158f6fac325446428d056eb920c6bb4ac42de2
maekawatoshiki <konndennsa@gmail.com>
2019-03-30 20:24:40 +0900

    console.log no longer uses printf internally

--- src/builtin.rs
+++ src/builtin.rs
@@ -78,15 +78,13 @@ pub fn parse_float(vm: &mut VM2, args: &[Value2], _cur_frame: &Frame) -> Result<
 pub fn builtin_log(vm: &mut VM2, args: &[Value2], _cur_frame: &Frame) -> Result<(), RuntimeError> {
     let args_len = args.len();
 
-    unsafe {
-        for i in 0..args_len {
-            debug_print2(&args[i], false);
-            if args_len - 1 != i {
-                libc::printf(b" \0".as_ptr() as RawStringPtr);
-            }
+    for i in 0..args_len {
+        debug_print2(&args[i], false);
+        if args_len - 1 != i {
+            print!(" ");
         }
-        libc::puts(b"\0".as_ptr() as RawStringPtr);
     }
+    println!();
 
     vm.stack.push(Value2::undefined().into());
 
@@ -96,196 +94,142 @@ pub fn builtin_log(vm: &mut VM2, args: &[Value2], _cur_frame: &Frame) -> Result<
 pub fn debug_print2(val: &Value2, nest: bool) {
     fn show_obj(sorted_key_val: Vec<(&String, &Property2)>) {
         for (i, tupple) in sorted_key_val.iter().enumerate() {
-            unsafe {
-                libc::printf(
-                    "'%s'\0".as_ptr() as RawStringPtr,
-                    CString::new(tupple.0.as_str()).unwrap().into_raw(),
-                );
-                libc::printf(": \0".as_ptr() as RawStringPtr);
+            print!("'{}': ", tupple.0.as_str());
 
-                match tupple.1 {
-                    Property2::Data(DataProperty { val, .. }) => {
-                        debug_print2(&val, true);
-                    }
-                    Property2::Accessor(AccessorProperty { get, set, .. }) => {
-                        let s_get = if get.is_undefined() { "\0" } else { "Getter\0" };
-                        let s_set = if set.is_undefined() { "\0" } else { "Setter\0" };
-                        libc::printf(
-                            "[%s%s%s]\0".as_ptr() as RawStringPtr,
-                            s_get.as_ptr() as RawStringPtr,
-                            if !get.is_undefined() && !set.is_undefined() {
-                                "/\0"
-                            } else {
-                                "\0"
-                            }
-                            .as_ptr() as RawStringPtr,
-                            s_set.as_ptr() as RawStringPtr,
-                        );
-                    }
+            match tupple.1 {
+                Property2::Data(DataProperty { val, .. }) => {
+                    debug_print2(&val, true);
                 }
-
-                libc::printf(if i != sorted_key_val.len() - 1 {
-                    ", \0".as_ptr() as RawStringPtr
-                } else {
-                    " \0".as_ptr() as RawStringPtr
-                });
+                Property2::Accessor(AccessorProperty { get, set, .. }) => {
+                    let s_get = if get.is_undefined() { "" } else { "Getter" };
+                    let s_set = if set.is_undefined() { "" } else { "Setter" };
+                    print!(
+                        "[{}{}{}]",
+                        s_get,
+                        if !get.is_undefined() && !set.is_undefined() {
+                            "/"
+                        } else {
+                            ""
+                        },
+                        s_set
+                    );
+                }
             }
+
+            print!(
+                "{}",
+                if i != sorted_key_val.len() - 1 {
+                    ", "
+                } else {
+                    " "
+                }
+            );
         }
     }
 
-    unsafe {
-        match val {
-            Value2::Other(UNINITIALIZED) => {
-                libc::printf(b"uninitialized\0".as_ptr() as RawStringPtr);
+    match val {
+        Value2::Other(UNINITIALIZED) => print!("uninitialized"),
+        Value2::Other(EMPTY) => print!("empty"),
+        Value2::Other(NULL) => print!("null"),
+        Value2::Other(UNDEFINED) => print!("undefined"),
+        Value2::Other(_) => unreachable!(),
+        Value2::Bool(1) => println!("true"),
+        Value2::Bool(0) => print!("false"),
+        Value2::Bool(_) => unreachable!(),
+        Value2::Number(n) if n.is_nan() => print!("NaN"),
+        Value2::Number(n) if n.is_infinite() => print!("Infinity"),
+        Value2::Number(n) => print!("{}", *n),
+        Value2::String(ref s) => {
+            let s = unsafe { &**s }.to_str().unwrap();
+            if nest {
+                print!("'{}'", s)
+            } else {
+                print!("{}", s)
             }
-            Value2::Other(EMPTY) => {
-                libc::printf(b"empty\0".as_ptr() as RawStringPtr);
-            }
-            // Value::Object(_, ObjectKind::Arguments(state)) => {
-            //     let args = &state.arguments;
-            //     libc::printf("[ \0".as_ptr() as RawStringPtr);
-            //
-            //     let mut i = 0;
-            //     let length = args.len();
-            //     while i < length {
-            //         if i != 0 {
-            //             libc::printf(", \0".as_ptr() as RawStringPtr);
-            //         };
-            //         match state.get_arguments_nth_value(i) {
-            //             Ok(val) => {
-            //                 debug_print(&val, true);
-            //             }
-            //             Err(_) => {
-            //                 libc::printf(" \0".as_ptr() as RawStringPtr);
-            //             }
-            //         };
-            //
-            //         i += 1;
-            //     }
-            //     libc::printf(" ]\0".as_ptr() as RawStringPtr);
-            // }
-            Value2::Other(NULL) => {
-                libc::printf(b"null\0".as_ptr() as RawStringPtr);
-            }
-            Value2::Other(UNDEFINED) => {
-                libc::printf(b"undefined\0".as_ptr() as RawStringPtr);
-            }
-            Value2::Other(_) => unreachable!(),
-            Value2::Bool(1) => {
-                libc::printf(b"true\0".as_ptr() as RawStringPtr);
-            }
-            Value2::Bool(0) => {
-                libc::printf(b"false\0".as_ptr() as RawStringPtr);
-            }
-            Value2::Bool(_) => unreachable!(),
-            Value2::Number(n) => {
-                if n.is_nan() {
-                    libc::printf("NaN\0".as_ptr() as RawStringPtr);
-                } else if n.is_infinite() {
-                    libc::printf("Infinity\0".as_ptr() as RawStringPtr);
-                } else {
-                    libc::printf("%.15g\0".as_ptr() as RawStringPtr, *n);
+        }
+        Value2::Object(obj_info) => {
+            let obj_info = unsafe { &**obj_info };
+
+            match obj_info.kind {
+                ObjectKind2::Ordinary => {
+                    print!("{{");
+
+                    let mut sorted_key_val = (&obj_info.property)
+                        .iter()
+                        .collect::<Vec<(&String, &Property2)>>();
+                    sorted_key_val.sort_by(|(key1, _), (key2, _)| key1.as_str().cmp(key2.as_str()));
+                    sorted_key_val.retain(|(ref key, _)| key != &"__proto__");
+
+                    show_obj(sorted_key_val);
+
+                    print!("}}");
                 }
-            }
-            Value2::String(ref s) => {
-                libc::printf(
-                    if nest { "'%s'\0" } else { "%s\0" }.as_ptr() as RawStringPtr,
-                    { &**s }.as_ptr(),
-                );
-            }
-            Value2::Object(obj_info) => {
-                let obj_info = &**obj_info;
-                match obj_info.kind {
-                    ObjectKind2::Ordinary => {
-                        libc::printf("{ \0".as_ptr() as RawStringPtr);
-
-                        let mut sorted_key_val =
-                            (&obj_info.property)
-                                .iter()
-                                .collect::<Vec<(&String, &Property2)>>();
-                        sorted_key_val
-                            .sort_by(|(key1, _), (key2, _)| key1.as_str().cmp(key2.as_str()));
-                        sorted_key_val.retain(|(ref key, _)| key != &"__proto__");
-
-                        show_obj(sorted_key_val);
-
-                        libc::printf("}\0".as_ptr() as RawStringPtr);
+                ObjectKind2::Function(ref func_info) => {
+                    if let Some(ref name) = func_info.name {
+                        print!("[Function: {}]", name);
+                    } else {
+                        print!("[Function]");
                     }
-                    ObjectKind2::Function(ref func_info) => {
-                        libc::printf(
-                            if let Some(ref name) = func_info.name {
-                                format!("[Function: {}]\0", name)
-                            } else {
-                                "[Function]\0".to_string()
-                            }
-                            .as_ptr() as RawStringPtr,
-                        );
-                    }
-                    ObjectKind2::Array(ref ary_info) => {
-                        libc::printf("[ \0".as_ptr() as RawStringPtr);
-                        let mut sorted_key_val =
-                            (&obj_info.property)
-                                .iter()
-                                .collect::<Vec<(&String, &Property2)>>();
-                        sorted_key_val
-                            .sort_by(|(key1, _), (key2, _)| key1.as_str().cmp(key2.as_str()));
-                        sorted_key_val.retain(|(ref key, _)| key != &"__proto__");
+                }
+                ObjectKind2::Array(ref ary_info) => {
+                    print!("[ ");
 
-                        let length = ary_info.elems.len();
-                        let is_last_idx = |idx: usize| -> bool { idx == length - 1 };
-                        let mut i = 0;
-                        while i < length {
-                            let mut empty_elems = 0;
-                            while i < length && Value2::empty() == ary_info.elems[i].as_data().val {
-                                empty_elems += 1;
-                                i += 1;
-                            }
+                    let mut sorted_key_val = (&obj_info.property)
+                        .iter()
+                        .collect::<Vec<(&String, &Property2)>>();
+                    sorted_key_val.sort_by(|(key1, _), (key2, _)| key1.as_str().cmp(key2.as_str()));
+                    sorted_key_val.retain(|(ref key, _)| key != &"__proto__");
 
-                            if empty_elems > 0 {
-                                libc::printf(
-                                    "<%u empty item%s>%s\0".as_ptr() as RawStringPtr,
-                                    empty_elems,
-                                    if empty_elems >= 2 { "s\0" } else { "\0" }.as_ptr()
-                                        as RawStringPtr,
-                                    if is_last_idx(i - 1) && sorted_key_val.len() == 0 {
-                                        " \0"
-                                    } else {
-                                        ", \0"
-                                    }
-                                    .as_ptr() as RawStringPtr,
-                                );
-
-                                if is_last_idx(i - 1) {
-                                    break;
-                                }
-                            }
-
-                            debug_print2(&ary_info.elems[i].as_data().val, true);
-                            libc::printf(
-                                if is_last_idx(i) && sorted_key_val.len() == 0 {
-                                    " \0"
-                                } else {
-                                    ", \0"
-                                }
-                                .as_ptr() as RawStringPtr,
-                            );
-
+                    let length = ary_info.elems.len();
+                    let is_last_idx = |idx: usize| -> bool { idx == length - 1 };
+                    let mut i = 0;
+                    while i < length {
+                        let mut empty_elems = 0;
+                        while i < length && Value2::empty() == ary_info.elems[i].as_data().val {
+                            empty_elems += 1;
                             i += 1;
                         }
 
-                        show_obj(sorted_key_val);
+                        if empty_elems > 0 {
+                            print!(
+                                "<{} empty item{}>{}",
+                                empty_elems,
+                                if empty_elems >= 2 { "s" } else { "" },
+                                if is_last_idx(i - 1) && sorted_key_val.len() == 0 {
+                                    " "
+                                } else {
+                                    ", "
+                                }
+                            );
 
-                        libc::printf("]\0".as_ptr() as RawStringPtr);
+                            if is_last_idx(i - 1) {
+                                break;
+                            }
+                        }
+
+                        debug_print2(&ary_info.elems[i].as_data().val, true);
+
+                        if is_last_idx(i) && sorted_key_val.len() == 0 {
+                            print!(" ")
+                        } else {
+                            print!(", ")
+                        }
+
+                        i += 1;
                     }
+
+                    show_obj(sorted_key_val);
+
+                    print!("]");
                 }
-            } // Value::Object(_, ObjectKind::Date(box time_val)) => {
-              //     // TODO: Date needs toString() ?
-              //     libc::printf(
-              //         "%s\0".as_ptr() as RawStringPtr,
-              //         CString::new(time_val.to_rfc3339()).unwrap().as_ptr(),
-              //     );
-              // }
-        }
+            }
+        } // Value::Object(_, ObjectKind::Date(box time_val)) => {
+          //     // TODO: Date needs toString() ?
+          //     libc::printf(
+          //         "%s\0".as_ptr() as RawStringPtr,
+          //         CString::new(time_val.to_rfc3339()).unwrap().as_ptr(),
+          //     );
+          // }
     }
 }
 
