9cc7f58f8cb21099913b6360df5de2d9817f8f81
maekawatoshiki <konndennsa@gmail.com>
2019-04-07 01:01:11 +0900

    Add Math

--- src/builtins/math.rs
+++ src/builtins/math.rs
@@ -1,7 +1,33 @@
-// use builtin::BuiltinJITFuncInfo;
-// use llvm::core::*;
-// use rand::random;
-// use std::ffi::CString;
-// use vm::value::*;
-// use vm::{error::RuntimeError, vm::VM};
-//
+use gc::MemoryAllocator;
+use rand::random;
+use rustc_hash::FxHashMap;
+use vm::{
+    frame,
+    jsvalue::prototype::ObjectPrototypes,
+    jsvalue::{
+        object::{DataProperty, ObjectInfo, ObjectKind2, Property2},
+        value::Value2,
+    },
+    vm,
+};
+
+pub fn math(
+    memory_allocator: &mut MemoryAllocator,
+    object_prototypes: &ObjectPrototypes,
+) -> Value2 {
+    let math_random = Value2::builtin_function(
+        memory_allocator,
+        object_prototypes,
+        "random".to_string(),
+        math_random,
+    );
+
+    make_normal_object!(memory_allocator, object_prototypes,
+        random => true, false, true: math_random
+    )
+}
+
+pub fn math_random(vm: &mut vm::VM2, _args: &[Value2], _cur_frame: &frame::Frame) -> vm::VMResult {
+    vm.stack.push(Value2::Number(random::<f64>()).into());
+    Ok(())
+}
