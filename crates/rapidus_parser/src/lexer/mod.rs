53da5f04de78a83fa6288d287b26cbd1741c0744
maekawatoshiki <maekawatoshiki1017@gmail.com>
2023-08-21 17:30:15 +0900

    rapidus_parser: Add lexer test 'lex_fizzbuzz'

--- crates/rapidus_parser/src/lexer/mod.rs
+++ crates/rapidus_parser/src/lexer/mod.rs
@@ -469,4 +469,23 @@ mod tests {
         }
         insta::assert_debug_snapshot!(tokens);
     }
+
+    #[test]
+    fn lex_fizzbuzz() {
+        let source = Source::new(
+            SourceName::FileName("test.js".into()),
+            r#"for (let i = 1; i < 20; i++) {
+    if (i % 15 == 0) console.log("FizzBuzz");
+    else if (i % 3 == 0) console.log("Fizz");
+    else if (i % 5 == 0) console.log("Buzz");
+    else console.log(i);
+}"#,
+        );
+        let mut lexer = Lexer::new(Input::from(&source));
+        let mut tokens = vec![];
+        while let Ok(Some(token)) = lexer.read_token() {
+            tokens.push(token);
+        }
+        insta::assert_debug_snapshot!(tokens);
+    }
 }
