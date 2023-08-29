78dcfc18495f8c1546245d36e3fd483f2791b68d
maekawatoshiki <maekawatoshiki1017@gmail.com>
2023-08-30 00:38:25 +0900

    rapidus_eval: Add tests

--- crates/rapidus_eval/tests/lit.rs
+++ crates/rapidus_eval/tests/lit.rs
@@ -24,3 +24,36 @@ fn str() {
     let val = EvalCtx::new().eval_module(&module).unwrap();
     assert_eq!(format!("{val:?}"), r#"str("hello")"#);
 }
+
+#[test]
+fn null() {
+    let src = Source::new(SourceName::Custom("test".into()), r#"null"#);
+    let module = Parser::new(Lexer::new(Input::from(&src)))
+        .parse_module()
+        .unwrap();
+    let val = EvalCtx::new().eval_module(&module).unwrap();
+    assert_eq!(val, JsValue::null());
+}
+
+#[test]
+fn arith() {
+    let src = Source::new(SourceName::Custom("test".into()), r#"12345+19382-113*91/2"#);
+    let module = Parser::new(Lexer::new(Input::from(&src)))
+        .parse_module()
+        .unwrap();
+    let val = EvalCtx::new().eval_module(&module).unwrap();
+    assert_eq!(val, JsValue::f64(26585.5));
+}
+
+#[test]
+fn let_binding() {
+    let src = Source::new(
+        SourceName::Custom("test".into()),
+        r#"let foo = 24; let bar = 18; foo + bar"#,
+    );
+    let module = Parser::new(Lexer::new(Input::from(&src)))
+        .parse_module()
+        .unwrap();
+    let val = EvalCtx::new().eval_module(&module).unwrap();
+    assert_eq!(val, JsValue::f64(42.0));
+}
